010fda41c10b931fa2ce79e2cd9d27e8093bf1a9
daniel nzioka <dmwinzi50@gmail.com>
2024-08-26 19:03:17 +0300

    code-challenge-1

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,28 @@
 import React, { useState } from "react";
-
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
-
+import data from "../db.json"
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
-
+  const onChangeType = (type) => {
+        setFilters({ ...filters, type });
+      };
+      const onFindPetsClick = () => {
+        let filteredPets;
+        if (filters.type === "all") {
+          filteredPets = data.pets;
+        } else {
+          filteredPets = data.pets.filter(pet => pet.type === filters.type);
+        }
+        setPets(filteredPets);
+      };
+      const onAdoptPet = (id) => {
+        const updatedPets = pets.map(pet =>
+          pet.id === id ? { ...pet, isAdopted: true } : pet
+        );
+        setPets(updatedPets);
+      };
   return (
     <div className="ui container">
       <header>
@@ -15,15 +31,14 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={onChangeType} onFindPetsClick={onFindPetsClick} />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={onAdoptPet}/>
           </div>
         </div>
       </div>
     </div>
   );
 }
-
 export default App;
